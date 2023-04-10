487054fe8ac2f8671eeb8721d1ce6d5c400f7e48
mac pro <macpro@macs-MacBook-Pro.local>
2023-04-10 12:05:16 +0800

    update datadiri

--- fun/WARNA.ts
+++ fun/WARNA.ts
@@ -2,5 +2,6 @@ const COLOR = {
     coklat: "#83281C",
     abuabu: "#D9D2CE",
     orange: "#E77617"
+
 }
 export default COLOR
--- src/layout/form_data_diri/form_data_diri.tsx
+++ src/layout/form_data_diri/form_data_diri.tsx
@@ -1,8 +1,16 @@
+import { BackgroundImage, Box, Center, Container, Grid, Image } from "@mantine/core"
 
 
 const FormDataDiri = () => {
-    return<>
-    Form Data Diri
+    return <>
+        <BackgroundImage
+            src="BG.png"
+            h={'100vh'}
+        >
+            <Grid>
+                <Grid.Col 
+            </Grid>
+        </BackgroundImage>
     </>
 }
 
