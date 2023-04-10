d2720afd4ddf007c48c1a212e4656d4883f50889
mac pro <macpro@macs-MacBook-Pro.local>
2023-04-10 12:03:51 +0800

    update register

---
+++ src/pages/register.tsx
@@ -0,0 +1,10 @@
+import Register from "@/layout/auth/form-register"
+
+const LayoutDefault = () => {
+    return (
+        <>
+        <Register/>
+        </>
+    )
+}
+export default LayoutDefault
