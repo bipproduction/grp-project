3cb62197db99dc3b3b878e483bc158045340ebeb
mac pro <macpro@macs-MacBook-Pro.local>
2023-04-10 12:01:41 +0800

    update login

--- src/layout/auth/form-login.tsx
+++ src/layout/auth/form-login.tsx
@@ -1,16 +1,40 @@
-import { Box, Button, Text, TextInput, Space, Anchor, NavLink, Container, Center } from "@mantine/core";
-
+import { Box, Button, Text, TextInput, Space, Anchor, NavLink, Container, Center, Image, Stack, Modal, BackgroundImage } from "@mantine/core";
+import COLOR from "../../../fun/WARNA";
+import { useDisclosure } from '@mantine/hooks';
+import Register from "./form-register";
 
 const Login = () => {
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <>
-      <Center  my={100}>
-        <Box bg={'gray.2'} h={500} w={500}>
-        <Text>Login Form</Text>
-        <Space h={10}/>
-        <Anchor href="../../dashboard">login</Anchor>
+      <BackgroundImage
+        src="BG.png"
+        h={'100vh'}
+      >
+        <Box>
+          <Center pt={130}>
+            <Box bg={COLOR.orange} h={500} w={500} sx={{
+              borderRadius: 10
+            }}>
+              <Text pt={10} ta={"center"} color="white" fz={60}>Garuda</Text>
+              <Center pt={10}>
+                <Image src={"logo.png"} width={200} />
+              </Center>
+              <Stack pt={20}>
+                <Container w={350}>
+                  <TextInput placeholder="Email" radius={10} />
+                  <TextInput mt={20} placeholder="Password" radius={10} />
+                  <Button mt={20} fullWidth radius={"lg"} component="a" href="../../../form_data_diri" bg={COLOR.coklat}>Login</Button>
+                  <Box component="a" href="../../../register">
+                  <Text style={{ cursor: "pointer" }} align="right" color="black"  mt={10} fz={10}><strong>Klik Disini,</strong> Untuk Daftar!</Text>
+                  </Box>
+                </Container>
+              </Stack>
+              {/* <Anchor href="../../dashboard">login</Anchor> */}
+            </Box>
+          </Center>
         </Box>
-      </Center>
+      </BackgroundImage>
     </>
   );
 };
