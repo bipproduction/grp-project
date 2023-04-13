b06dab659b25deaf65fef3c2ea2d52fd566f73ed
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-04-13 14:13:21 +0800

    Tambah Api GET user role

--- src/lib/api-get.ts
+++ src/lib/api-get.ts
@@ -1,3 +1,8 @@
 export const ApiGetAll = {
-    
+    // Profile
+    "Agama": "",
+    // Sumber Daya Partai
+    // Peta Kekuatan
+    // Aksi Nyata
+
 }
--- src/pages/api/get/get-api.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-
-const GetAllApi = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200)
-
-}
-
-export default GetAllApi
