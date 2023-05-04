5f665b339a5643a09b69494ea89a8abc30b05f16
lukman <lukman@lukmans-MacBook-Air.local>
2023-05-04 10:34:44 +0800

    update

--- fun/WARNA.ts
+++ fun/WARNA.ts
@@ -4,11 +4,6 @@ const COLOR = {
     abuabu: "#D9D2CE",
     hitam: "#25262B",
     ungu: "#0099CA",
-    coklat: '#83281C',
-    abu2: "#AEAEAE",
-    hitam1: "#525252",
-    biru: "#332B50"
-
 
 }
 export default COLOR
