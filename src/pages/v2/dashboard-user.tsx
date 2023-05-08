6b8de4e30551b841cf75e6b1411494ba53e7c33d
amel <amel@amel.local>
2023-05-08 10:33:07 +0800

    api wilayah

--- src/pages/v2/dashboard-user.tsx
+++ src/pages/v2/dashboard-user.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 const DashboardUserV2 = () => {
     const router = useRouter();
     //if (sUser.value == undefined) return <></>;
-    //if ( sUser.value.masterUserRoleId != "1") router.replace("/v2/");
+    if ( sUser.value.masterUserRoleId != "1") router.replace("/");
 
     return <>
         <LayoutDashboardUserV2 />
