e6fbf98371b079dfbe5d54ac600227129888c897
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-05-25 11:12:33 +0800

    Edit struktur on progres

---
+++ src/model/interface_tingkat_pengurus.ts
@@ -0,0 +1,23 @@
+export interface ModelTingkatPengurus {
+    userId: string
+    masterStatusKeanggotaanId: number
+    masterTingkatPengurusId: number
+    masterJabatanId: any
+    masterJabatanDewanPembinaId: number
+    masterJabatanDewanPimpinanPusatId: number
+    masterJabatanDewanPimpinanDaerahId: number
+    masterJabatanDewanPimpinanCabangId: number
+    masterJabatanPimpinanAnakCabangId: number
+    masterJabatanPimpinanRantingId: number
+    masterJabatanPerwakilanPartaiDiLuarNegeriId: number
+    masterSayapPartaiId: number
+    masterKaderPartaiId: number
+    masterProvinceId: number
+    masterKabKotId: number
+    masterKecamatanId: number
+    masterDesaId: number
+    masterNegaraId: number
+    alamatKantor: string
+    waAdmin: string
+  }
+  
