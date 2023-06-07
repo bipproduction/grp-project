2d82dc0434c076f33177049bccc8ce387ab6ff28
lukman <lukman@lukmans-MacBook-Air.local>
2023-06-07 21:07:36 +0800

    update pembaruan api image

--- src/pages/api/image-upload/upload.ts
+++ src/pages/api/image-upload/upload.ts
@@ -9,11 +9,15 @@ export default async function handler(req: any, res: any){
     })
   }
   try {
+    let img;
     const data = await new Promise(function(resulve, reject){
       const form = new formidable.IncomingForm({
         keepExtensions: true,
         uploadDir: "./images",
-        filename: (v) => v+".png",
+        filename: (v) => {
+          img = v+".png"
+          return img
+        },
       });
       form.parse(req,function (err, fields, files){
         if (err)  return reject(err)
@@ -21,7 +25,7 @@ export default async function handler(req: any, res: any){
       })
     }) 
     console.log(data)
-    res.status(201).json({success: true})
+    res.status(201).json({success: true, img: img})
   } catch (e) {
     res.status(400).end()
   }
