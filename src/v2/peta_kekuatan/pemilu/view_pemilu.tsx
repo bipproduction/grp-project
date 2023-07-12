550d67a0cc3197f5fa740b7653afeb1e48f517d2
Bagasbanuna02 <bagasbanuna02@gmail.com>
2023-07-12 12:03:12 +0800

    Data Pemilu done

--- src/v2/peta_kekuatan/pemilu/view_pemilu.tsx
+++ src/v2/peta_kekuatan/pemilu/view_pemilu.tsx
@@ -11,20 +11,14 @@ import {
   Loader,
   Title,
   Button,
+  ActionIcon,
+  Group,
 } from "@mantine/core";
 import COLOR from "../../../../fun/WARNA";
 import { api } from "@/lib/api-backend";
 import { useShallowEffect } from "@mantine/hooks";
 import { useState } from "react";
 import _, { isEmpty } from "lodash";
-import toast from "react-simple-toasts";
-
-interface ModelProv {
-  id: number;
-  name: string;
-  value1: string;
-  value2: string;
-}
 
 export const ViewPemiluV2 = () => {
   const [prov, setProv] = useState<any[]>([]);
@@ -47,17 +41,11 @@ export const ViewPemiluV2 = () => {
     id: new Number(),
     name: "",
   });
-
-  const [refresh, setRefresh] = useState("1");
-
-  useShallowEffect(() => {
-    dataPemilu();
-  }, []);
+  const [selectNasional, setSelectNasional] = useState<any>();
+  const [nomorTb, setNomorTb] = useState("0");
 
   useShallowEffect(() => {
-    if (refresh) {
-      dataPemilu();
-    }
+    dataPemilu(setSelectProv, setSelectKabkot, setSelectKecamatan);
   }, []);
 
   async function refreshProv() {
@@ -68,11 +56,18 @@ export const ViewPemiluV2 = () => {
       });
   }
 
-  const dataPemilu = async () => {
+  const dataPemilu = async (
+    setSelectProv: any,
+    setSelectKabkot: any,
+    setSelectKecamatan: any
+  ) => {
     await fetch(api.apiPemiluGetData)
       .then((res) => res.json())
       .then(async (val) => {
         setProv(val);
+        setSelectProv({});
+        setSelectKabkot({});
+        setSelectKecamatan({});
       });
   };
 
@@ -116,6 +111,14 @@ export const ViewPemiluV2 = () => {
       .then((val) => setDesa(val));
   };
 
+  const tableHead = (
+    <tr>
+      <th>Wilayah</th>
+      <th>Ir. H. Joko Widodo</th>
+      <th>Prabowo Subianto Djojohadikusumo</th>
+    </tr>
+  );
+
   const tableBodyProv = prov.map((e) => (
     <tr key={e.id}>
       <td>{e.name}</td>
@@ -149,16 +152,12 @@ export const ViewPemiluV2 = () => {
   ));
   return (
     <>
-      {/* {JSON.stringify(selectProv.id)}
-      <Button onClick={() => setRefresh(Math.random().toString())}>Klik</Button>
-      {JSON.stringify(refresh)} */}
-
       <Box>
         <Paper bg={COLOR.abuabu} p={10}>
           <Grid>
             <Grid.Col span={8}>
               <Text size={20} fw={"bold"}>
-                Data Pemilu
+                Data Pemilu 2019
               </Text>
             </Grid.Col>
           </Grid>
@@ -167,147 +166,139 @@ export const ViewPemiluV2 = () => {
         {/* Select Wilayah */}
         <Box pt={20}>
           <Paper bg={COLOR.abuabu} p={10} radius={30}>
-            <SimpleGrid cols={3}>
-              <Select
-                clearable={selectProv.id == undefined ? true : false}
-                value={
-                  (selectProv.id as any)
-                    ? (selectProv.id as any)
-                    : "Pilih Provinsi"
-                }
-                placeholder={
-                  selectProv.name ? selectProv.name : "Pilih Provinsi"
-                }
-                radius={20}
-                searchable
-                data={prov.map((e) => ({
-                  value: e.id,
-                  label: e.name,
-                }))}
-                onChange={(val) => {
-                  setSelectProv(prov.find((e) => e.id == val));
-                  dataPemiluKabkot(val, setSelectKabkot, setSelectKecamatan);
-                  setRefresh(val as any);
-                }}
-              />
-              <Select
-                value={
-                  (selectKabkot.id as any)
-                    ? (selectKabkot.id as any)
-                    : "Pilih Kabupaten / Kota"
-                }
-                placeholder={
-                  selectKabkot.name
-                    ? selectKabkot.name
-                    : "Pilih Kabupaten / Kota"
-                }
-                disabled={selectProv.id == 0 ? true : false}
-                radius={20}
-                data={kabkot.map((e) => ({
-                  value: e.id,
-                  label: e.name,
-                }))}
-                onChange={(val) => {
-                  setSelectKabkot(kabkot.find((e) => e.id == val));
-                  dataPemiluKecamatan(selectProv, val, setSelectKecamatan);
-                }}
-              />
-              <Select
-                value={
-                  (selectKecamatan.id as any)
-                    ? (selectKecamatan.id as any)
-                    : "Pilih Kecamatan"
-                }
-                placeholder={
-                  selectKecamatan.name
-                    ? selectKecamatan.name
-                    : "Pilih Kecamatan"
-                }
-                disabled={selectKabkot.id == 0 ? true : false}
-                radius={20}
-                data={kec.map((e) => ({
-                  value: e.id,
-                  label: e.name,
-                }))}
-                onChange={(val) => {
-                  setSelectKecamatan(kec.find((e) => e.id == val));
-                  dataPemiluDesa(prov, kabkot, val);
-                }}
-              />
-              {/* <Select
-                placeholder="Desa"
-                radius={20}
-                data={desa.map((e) => ({
-                  value: e.id,
-                  label: e.name,
-                }))}
-                onChange={(val) => {
-                  setSelectDesa(desa.find((e) => e.id == val));
-                }}
-              /> */}
-            </SimpleGrid>
+            <Grid grow justify="center" align="center">
+              <Grid.Col span={3}>
+                <Select
+                  value={selectNasional ? selectNasional : "Nasional"}
+                  placeholder="Nasional"
+                  radius={20}
+                  data={[{ value: "1", label: "Nasional" }]}
+                  onChange={(val) => {
+                    setSelectNasional(val);
+                    dataPemilu(
+                      setSelectProv,
+                      setSelectKabkot,
+                      setSelectKecamatan
+                    );
+                    setNomorTb("0");
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col span={3}>
+                <Select
+                  value={
+                    (selectProv.id as any)
+                      ? (selectProv.id as any)
+                      : "Pilih Provinsi"
+                  }
+                  placeholder={
+                    selectProv.name ? selectProv.name : "Pilih Provinsi"
+                  }
+                  radius={20}
+                  searchable
+                  data={prov.map((e) => ({
+                    value: e.id,
+                    label: e.name,
+                  }))}
+                  onChange={(val) => {
+                    // console.log(val)
+                    setSelectProv(prov.find((e) => e.id == val));
+                    dataPemiluKabkot(val, setSelectKabkot, setSelectKecamatan);
+                    setNomorTb("1");
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col span={3}>
+                <Select
+                  value={
+                    (selectKabkot.id as any)
+                      ? (selectKabkot.id as any)
+                      : "Pilih Kabupaten / Kota"
+                  }
+                  placeholder={
+                    selectKabkot.name
+                      ? selectKabkot.name
+                      : "Pilih Kabupaten / Kota"
+                  }
+                  disabled={!selectProv.id ? true : false}
+                  searchable
+                  radius={20}
+                  data={kabkot.map((e) => ({
+                    value: e.id,
+                    label: e.name,
+                  }))}
+                  onChange={(val) => {
+                    setSelectKabkot(kabkot.find((e) => e.id == val));
+                    dataPemiluKecamatan(selectProv, val, setSelectKecamatan);
+                    setNomorTb("2");
+                  }}
+                />
+              </Grid.Col>
+              <Grid.Col span={3}>
+                <Select
+                  value={
+                    (selectKecamatan.id as any)
+                      ? (selectKecamatan.id as any)
+                      : "Pilih Kecamatan"
+                  }
+                  placeholder={
+                    selectKecamatan.name
+                      ? selectKecamatan.name
+                      : "Pilih Kecamatan"
+                  }
+                  disabled={!selectKabkot.id ? true : false}
+                  searchable
+                  radius={20}
+                  data={kec.map((e) => ({
+                    value: e.id,
+                    label: e.name,
+                  }))}
+                  onChange={(val) => {
+                    setSelectKecamatan(kec.find((e) => e.id == val));
+                    dataPemiluDesa(prov, kabkot, val);
+                    setNomorTb("3");
+                  }}
+                />
+              </Grid.Col>
+            </Grid>
           </Paper>
         </Box>
 
         {/* Tampilan Table */}
         <Box pt={20}>
           {(() => {
-            if (isEmpty(kabkot)) {
+            if (nomorTb == "0") {
               return (
                 <>
                   <Table withColumnBorders withBorder>
-                    <thead>
-                      <tr>
-                        <th>Wilayah</th>
-                        <th>Jokowi</th>
-                        <th>Prabowo</th>
-                      </tr>
-                    </thead>
+                    <thead>{tableHead}</thead>
                     <tbody>{tableBodyProv}</tbody>
                   </Table>
                 </>
               );
-            } else if (isEmpty(kec)) {
+            } else if (nomorTb == "1") {
               return (
                 <>
                   <Table withColumnBorders withBorder>
-                    <thead>
-                      <tr>
-                        <th>Wilayah</th>
-                        <th>Jokowi</th>
-                        <th>Prabowo</th>
-                      </tr>
-                    </thead>
+                    <thead>{tableHead}</thead>
                     <tbody>{tableBodyKabkot}</tbody>
                   </Table>
                 </>
               );
-            } else if (isEmpty(desa) && !isEmpty(kec)) {
+            } else if (nomorTb == "2") {
               return (
                 <>
                   <Table withColumnBorders withBorder>
-                    <thead>
-                      <tr>
-                        <th>Wilayah</th>
-                        <th>Jokowi</th>
-                        <th>Prabowo</th>
-                      </tr>
-                    </thead>
+                    <thead>{tableHead}</thead>
                     <tbody>{tableBodyKecamatan}</tbody>
                   </Table>
                 </>
               );
-            } else if (!isEmpty(prov) && !isEmpty(kabkot) && !isEmpty(kec)) {
+            } else if (nomorTb == "3") {
               return (
                 <>
                   <Table withColumnBorders withBorder>
-                    <thead>
-                      <tr>
-                        <th>Wilayah</th>
-                        <th>Jokowi</th>
-                        <th>Prabowo</th>
-                      </tr>
-                    </thead>
+                    <thead>{tableHead}</thead>
                     <tbody>{tableBodyDesa}</tbody>
                   </Table>
                 </>
